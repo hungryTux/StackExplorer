37864f2f1e6c2abdf6e16dbdad1ce6968d72f672
bharath <bharath.yarlagadda89@gmail.com>
2013-03-16 20:50:37 -0400

    unanswered page

---
+++ unanswered.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title></title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body class="question-page">
+
+        <div class="container" style="width:990px;">
+            <div id="header" style="float:left;">
+                <div id="topbar">
+                <br class="cbt"/>
+                <br class="cbt"/>
+                <div id="hlogo" style="float:left;margin:57px 12px 0 0;overflow:hidden;width:200px">
+                    <a href="/">Stack Explorer</a>
+                </div>
+                <div id="hmenus" style="margin:57px 12px 0 0;overflow:hidden;">
+                    <div class="nav mainnavs" >
+                        <ul>
+                            <li class="youarehere" >
+                                <a id="nav-questions" href="/questions">Questions</a>
+                            </li>
+                            <li>
+                                <a id="nav-tags" href="/tags">Tags</a>
+                            </li>
+                            <li>
+                                <a id="nav-users" href="/users">Users</a>
+                            </li>
+                            <li>
+                                <a id="nav-badges" href="/badges">Badges</a>
+                            </li>
+                            <li>
+                                <a id="nav-unanswered" href="/unanswered">Unanswered</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="content" style="float:left;width:930px;">
+                <div itemscope="" itemtype="http://schema.org/Article">
+                    <div id="question-header">
+                    <h1 itemprop="name">
+                        <a href="/questions/15094/how-to-make-my-node-object-properties-available-in-my-views-template-also" class="question-hyperlink">How to make my $node object properties available in my views— template also?</a>
+                    </h1>
+                    </div>
+                    
+                    <div id="mainbar" style="float:left;width:700px;">
+                        <div class="question" data-questionid="15094" id="question">
+                        <table>
+                            <tbody><tr>
+                                <td class="votecell">
+                                    <div class="vote">
+                                        <input type="hidden" value="15094">
+                                        <a class="vote-up-off" title="This question shows research effort; it is useful and clear (click again to undo)">up vote</a>
+                                        <span class="vote-count-post ">2</span>
+                                        <a class="vote-down-off" title="This question does not show any research effort; it is unclear or not useful (click again to undo)">down vote</a>
+                                    </div>
+                                </td>
+                                <td class="postcell">
+                                    <div>
+                                        <div class="post-text" itemprop="description">
+                                            <p>I have node-- type templates and also view-- type templates.The main problem is while in view-- template I cannot access <code>$node</code> variables for instance:</p>
+                                            <p><code>$node-&gt;nid</code> etc</p>
+                                            <p>How can I inject <code>$node</code> variables inside view type templates?</p>
+                                            <p>For further info I have a views-view-field--mypage--page--body.tpl.php template, this is a field template of the body of teasers where I inserted a readmore link. I need the url for read more link.</p>
+                                            <p>Thanks</p>
+                                        </div>
+                                        <div class="post-taglist">
+                                            <a href="/questions/tagged/views" class="post-tag" title="show questions tagged 'views'" rel="tag">views</a> <a href="/questions/tagged/preprocess" class="post-tag" title="" rel="tag">preprocess</a> 
+                                        </div>
+                                        <table class="fw">
+                                            <tbody><tr>
+                                                <td class="vt">
+                                                    <div class="post-menu">
+                                                        <a href="/q/15094" title="short permalink to this question" class="short-link" id="link-post-15094">share</a>
+                                                        <span class="lsep">|</span>
+                                                        <a href="/posts/15094/edit" class="suggest-edit-post" title="">improve this question</a>
+                                                    </div>
+                                                </td>
+                                                <td align="right" class="post-signature">
+                                                    <div class="user-info user-hover">
+                                                        <div class="user-action-time">
+                                                            edited
+                                                            <a href="/posts/15094/revisions" title="show all edits to this post"><span title="2011-11-09 13:54:39Z" class="relativetime">Nov 9 '11 at 13:54</span></a>
+
+                                                         </div>
+                                                        <div class="user-gravatar32">
+                                                            <a href="/users/320/laxman13"><div class=""><img src="http://www.gravatar.com/avatar/bd9b9464b06f7d247df674c1f361fb2e?s=32&amp;d=identicon&amp;r=PG" alt="" width="32" height="32"></div></a>
+                                                        </div>
+                                                        <div class="user-details">
+                                                            <a href="/users/320/laxman13">Laxman13</a><br>
+                                                            <span class="reputation-score" title="reputation score" dir="ltr">4,659</span><span title="2 gold badges"><span class="badge1"></span><span class="badgecount">2</span></span><span title="10 silver badges"><span class="badge2"></span><span class="badgecount">10</span></span><span title="27 bronze badges"><span class="badge3"></span><span class="badgecount">27</span></span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="post-signature owner">
+                                                    <div class="user-info ">
+                                                        <div class="user-action-time">
+                                                            asked
+                                                            <span title="2011-11-09 12:02:45Z" class="relativetime">Nov 9 '11 at 12:02</span>
+                                                        </div>
+                                                        <div class="user-gravatar32">
+                                                            <a href="/users/3143/jacques-brel"><div class=""><img src="http://www.gravatar.com/avatar/f4c3e025983f3dbfdb2a095af12f3256?s=32&amp;d=identicon&amp;r=PG" alt="" width="32" height="32"></div></a>
+                                                        </div>
+                                                        <div class="user-details">
+                                                            <a href="/users/3143/jacques-brel">Jacques Brel</a><br>
+                                                            <span class="reputation-score" title="reputation score" dir="ltr">1,137</span><span title="2 silver badges"><span class="badge2"></span><span class="badgecount">2</span></span><span title="17 bronze badges"><span class="badge3"></span><span class="badgecount">17</span></span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr></tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="votecell"></td>
+                                <td>
+                                <div id="comments-15094" class="comments">
+                                    <table>
+                                        <tbody>
+                                            <tr id="comment-13965" class="comment">
+                                                <td></td>
+                                                <td class="comment-text"><div><span class="comment-copy">Crossed to: <a href="http://stackoverflow.com/questions/8065354/drupal7-how-to-make-my-node-object-properties-available-in-my-views-template" title="drupal7 how to make my node object properties available in my views template">stackoverflow.com/questions/8065354/…</a></span> –&nbsp;<a href="/users/320/laxman13" title="4659 reputation" class="comment-user">Laxman13</a> <span class="comment-date" dir="ltr"><a class="comment-link" onclick="StackExchange.question.highlightComment(13965, 0)" href="#comment13965_15094"><span title="2011-11-09 13:53:49Z" class="relativetime-clean">Nov 9 '11 at 13:53</span></a></span></div></td>
+                                            </tr>
+                                            <tr id="comment-17922" class="comment">
+                                                <td class="comment-actions">
+                                                    <table><tbody><tr>
+                                                        <td class="comment-score">
+                                                            <span title="number of 'useful comment' votes received" class="cool">1</span>
+                                                        </td>
+                                                        <td>&nbsp;
+                                                        </td></tr>
+                                                    </tbody></table>
+                                                </td>
+                                                <td class="comment-text"><div><span class="comment-copy">If the node-&gt;object isn't loaded in a specific template, it probably shouldn't be there. I think what you really should consider is if something is wrong with your approach to the problem.</span> –&nbsp;<a href="/users/1625/letharion" title="12447 reputation" class="comment-user">Letharion</a> <span class="comment-date" dir="ltr"><a class="comment-link" onclick="StackExchange.question.highlightComment(17922, 0)" href="#comment17922_15094"><span title="2012-01-08 17:22:30Z" class="relativetime-clean">Jan 8 '12 at 17:22</span></a></span></div></td>
+                                            </tr>
+                                        </tbody>    
+                                    </table>
+                                </div> 
+                                </td>
+                            </tr></tbody>
+                        </table>    
+                        </div>
+                        <div class="answer-form">
+                            <form id="post-form" action="/questions/15094/answer/submit" method="post" class="post-form">
+                                <h2 class="space">Your Answer</h2>
+                                <div id="post-editor" class="post-editor">
+                                    <div >
+                                        <div class="wmd-container">
+                                            <textarea id="wmd-input" class="wmd-input processed" name="post-text" cols="92" rows="15" tabindex="101" data-min-length=""></textarea>
+                                            <div class="grippie" style="margin-right: 0px;"></div>
+                                        </div>
+                                        <div class="fl" style="margin-top: 8px; height:24px;">&nbsp;</div>
+                                        <div> 
+                                        <div class="form-item">
+                                            <table>
+                                                <tbody><tr>
+                                                    <td class="vm">
+                                                        <div>
+                                                            <label for="display-name">Name</label>
+                                                            <input type="text" size="30" maxlength="30" value="" tabindex="105" style="opacity: 1; position: absolute; background-color: white; color: black; -webkit-text-fill-color: black; width: 205px; height: 15px; line-height: normal; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 13px; text-align: start; border: 1px solid rgb(204, 204, 204);" class="actual-edit-overlay" disabled="disabled"><input id="display-name" name="display-name" type="text" size="30" maxlength="30" value="" tabindex="105" style="opacity: 0.4; z-index: 1; position: relative;" class="edit-field-overlayed">
+                                                        </div>
+                                                        <div>
+                                                            <label for="m-address">Email</label>
+                                                            <input type="text" size="40" maxlength="100" value="" tabindex="106" style="opacity: 1; position: absolute; background-color: white; color: black; -webkit-text-fill-color: black; width: 265px; height: 15px; line-height: normal; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 13px; text-align: start; border: 1px solid rgb(204, 204, 204);" class="actual-edit-overlay" disabled="disabled"><input id="m-address" name="m-address" type="text" size="40" maxlength="100" value="" tabindex="106" style="opacity: 0.4; z-index: 1; position: relative;" class="edit-field-overlayed">
+                                                        </div>
+                                                        <div>
+                                                            <label for="home-page">Home Page</label>
+                                                            <input type="text" size="40" maxlength="200" value="" tabindex="107" style="opacity: 1; position: absolute; background-color: white; color: black; -webkit-text-fill-color: black; width: 265px; height: 15px; line-height: normal; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 13px; text-align: start; border: 1px solid rgb(204, 204, 204);" class="actual-edit-overlay" disabled="disabled"><input id="home-page" name="home-page" type="text" size="40" maxlength="200" value="" tabindex="107" style="opacity: 0.4; z-index: 1; position: relative;" class="edit-field-overlayed">
+                                                        </div>
+                                                    </td>
+                                                </tr></tbody>
+                                            </table>
+                                        </div>
+                                        </div>
+                                        <div class="form-submit cbt">
+                                            <input id="submit-button" type="submit" value="Post Your Answer" tabindex="110">
+                                            <a href="#" class="discard-answer dno">discard</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        </div>
+                    
+                    <div id="sidebar" style="float:right;width:220px;">
+                    <div class="module newuser newuser-greeting" id="newuser-box" style="">
+                        <h4>Welcome!</h4>
+                        <div>
+                            <p>This is a question and answer site for 
+                                <b>stack explorer developers</b>. It's 100% free, no registration or login required.</p><p>Got a question about the site itself?  can't help right now.</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+            </div>
+        </div>
+    </body>
+</html>
